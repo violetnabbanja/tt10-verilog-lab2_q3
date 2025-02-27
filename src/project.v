2a8dc3245a5b014210bfdf384816b7575554b7a7
violetnabbanja <violet.nabbanja@gmail.com>
2025-02-27 15:17:06 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,27 +6,27 @@
 `default_nettype none
 
 module tt_um_lab2_q3 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,   // Dedicated inputs
+    output wire [7:0] uo_out,  // Dedicated outputs
+    input  wire [7:0] uio_in,  // IOs: Input path
+    output wire [7:0] uio_out, // IOs: Output path
+    output wire [7:0] uio_oe,  // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire ena,           // Always 1 when the design is powered, so you can ignore it
+    input  wire clk,           // Clock
+    input  wire rst_n          // Reset_n - low to reset
 );
 
+// Implement bitwise AND for lower 7 bits
+assign uo_out[6:0] = ui_in[6:0] & uio_in[6:0];
 
-    // Implement bitwise AND for lower 7 bits
-    assign uo_out[6:0] = ui_in[6:0] & uio_in[6:0];
+// Conditional MSB toggling
+assign uo_out[7] = (ui_in[7] == uio_in[7]) ? ui_in[7] : ~ui_in[7];
 
-    // Conditional MSB toggling
-    assign uo_out[7] = (ui_in[7] == uio_in[7]) ? ui_in[7] : ~ui_in[7];
-        
-    assign uio_out = 0;
-    assign uio_oe  = 0;
+// Assign unused outputs to 0
+assign uio_out = 0;
+assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+// List all unused inputs to prevent warnings
+wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
